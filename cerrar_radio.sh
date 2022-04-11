880099da85fbc446117a822343bdc8f4d5387317
manel1954 <ea3eiz@ea3eiz.com>
2022-04-11 20:21:18 +0200

    1

--- cerrar_radio.sh
+++ cerrar_radio.sh
@@ -2,14 +2,14 @@
 sudo killall MMDVMHost
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
-sudo cp Abrir_Radio.desktop /home/pi
+cp Abrir_Radio.desktop /home/pi
 
-sudo sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;xterm -geometry 88x17+23+664 -bg black -fg white -fa 'Verdana' -fs 9x -T RADIO -e sudo sh OpenEjecutarRadio.sh'" /home/pi/Abrir_Radio.desktop
-sudo sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_RADIO_OFF.png" /home/pi/Abrir_Radio.desktop
-sudo sed -i "10c Name[es_ES]=Abrir RADIO" /home/pi/Abrir_Radio.desktop
-sudo sed -i "5c MMDVM=OFF" /home/pi/status.ini
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;xterm -geometry 88x17+23+664 -bg black -fg white -fa 'Verdana' -fs 9x -T RADIO -e sudo sh OpenEjecutarRadio.sh'" /home/pi/Abrir_Radio.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_RADIO_OFF.png" /home/pi/Abrir_Radio.desktop
+sed -i "10c Name[es_ES]=Abrir RADIO" /home/pi/Abrir_Radio.desktop
+sed -i "5c MMDVM=OFF" /home/pi/status.ini
 
 cd /home/pi
-sudo cp Abrir_Radio.desktop /home/pi/Desktop
+cp Abrir_Radio.desktop /home/pi/Desktop
 
-sudo rm /home/pi/Abrir_Radio.desktop
+rm /home/pi/Abrir_Radio.desktop

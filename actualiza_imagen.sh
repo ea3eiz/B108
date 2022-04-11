14fb6715476c736e47d1ee90251ef2e415711986
manel1954 <ea3eiz@ea3eiz.com>
2022-04-11 18:31:53 +0200

    1

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -41,52 +41,23 @@ MARRON="\33[38;5;138m"
                         exit
                         fi
 
-                        # Actualiza AUTORRANQUE_B108 
-                        #cd /home/pi/.local
-                        #git clone http://github.com/ea3eiz/AUTOARRANQUE_B108
-                        #sleep 2
-                        #if [ -d /home/pi/.local/AUTOARRANQUE_B108 ]
-                        #then
-                        #sudo rm -R /home/pi/AUTOARRANQUE_B108
-                        #cp -R /home/pi/.local/AUTOARRANQUE_B108 /home/pi
-                        #cd /home/pi
-                        #sudo chmod 777 -R AUTOARRANQUE_B108
-                        #cd /home/pi/.local
-                        #sudo rm -R AUTOARRANQUE_B108
-                        #else
-                        #echo "Error de red"
-                        #exit
-                        #fi
+
 
                         #modificación 01-01-2022
-                        sed -i "4c Exec=sh -c 'cd /home/pi/B108; ./qt_activar_nextiondriver'" /home/pi/.local/Activar_NextionDriver.desktop
-                        sed -i "5c Icon=//home/pi/B108/ICONO_NEXTIONDRIVER_OFF.png" /home/pi/.local/Activar_NextionDriver.desktop
+                        #sed -i "4c Exec=sh -c 'cd /home/pi/B108; ./qt_activar_nextiondriver'" /home/pi/.local/Activar_NextionDriver.desktop
+                        #sed -i "5c Icon=//home/pi/B108/ICONO_NEXTIONDRIVER_OFF.png" /home/pi/.local/Activar_NextionDriver.desktop
 
-                        sed -i "4c Exec=sh -c 'cd /home/pi/B108; ./qt_desactivar_nextiondriver'" /home/pi/.local/Desactivar_NextionDriver.desktop
-                        sed -i "5c Icon=//home/pi/B108/ICONO_NEXTIONDRIVER_ON.png" /home/pi/.local/Desactivar_NextionDriver.desktop
+                        #sed -i "4c Exec=sh -c 'cd /home/pi/B108; ./qt_desactivar_nextiondriver'" /home/pi/.local/Desactivar_NextionDriver.desktop
+                        #sed -i "5c Icon=//home/pi/B108/ICONO_NEXTIONDRIVER_ON.png" /home/pi/.local/Desactivar_NextionDriver.desktop
 
-                        sudo sed -i "27c 30 10 * * * sudo sh /home/pi/B108/actualiza_imagen.sh" /var/spool/cron/crontabs/pi
-
-                        sed -i "4c Exec=sh -c 'cd /home/pi/B108; ./qt_indicativo'" /home/pi/.config/autostart/indicativo.desktop
+                        #sed -i "4c Exec=sh -c 'cd /home/pi/B108; ./qt_indicativo'" /home/pi/.config/autostart/indicativo.desktop
 
                         # modificacion 17-01-2022
-                        sudo sed -i "1c 01-01-2022" /home/pi/.config/autostart/actualizacion
+                        #sudo sed -i "1c 01-01-2022" /home/pi/.config/autostart/actualizacion
 
-
-                        # modificacion 17-01-2022
-                        #cd /home/pi/B108
-                        #cp index_template.html_00 /opt/HBmonitor
-                        #cp index_template.html_01 /opt/HBmonitor
-                        #cp index_template.html_02 /opt/HBmonitor
-                        #cp index_template.html_03 /opt/HBmonitor
-                        #cp index_template.html_04 /opt/HBmonitor
-                        #cp monitor.py_00 /opt/HBmonitor
-                        #cp monitor.py_01 /opt/HBmonitor
-                        #cp monitor.py_02 /opt/HBmonitor
-                        #cp monitor.py_03 /opt/HBmonitor
-                        #cp monitor.py_04 /opt/HBmonitor
-
-                        # 26-08-2020 actualizar salas DSTAR
+                        
+                        
+                        # actualizar salas DSTAR
                         cd /usr/share/opendv/
                         sudo curl --fail -o DExtra_Hosts.txt -s http://www.pistar.uk/downloads/DExtra_Hosts.txt
                         sleep 1
@@ -94,7 +65,7 @@ MARRON="\33[38;5;138m"
                         sleep 1
                         sudo curl --fail -o DPlus_Hosts.txt -s http://www.pistar.uk/downloads/DPlus_Hosts.txt
 
-                        # 26-08-2020 actualizar salas DSTAR
+                        # actualizar salas DSTAR
                         cd /usr/local/share/opendv/
                         sudo curl --fail -o DExtra_Hosts.txt -s http://www.pistar.uk/downloads/DExtra_Hosts.txt
                         sleep 1
@@ -102,7 +73,7 @@ MARRON="\33[38;5;138m"
                         sleep 1
                         sudo curl --fail -o DPlus_Hosts.txt -s http://www.pistar.uk/downloads/DPlus_Hosts.txt
 
-                        # 26-08-2020 actualizar salas dv4mini
+                        # actualizar salas dv4mini
                         sudo cp DExtra_Hosts.txt $usuario/dv4mini/xref.ip
 
                         #Lee el fichero INFO_RXF para poner los datos en los iconos INFO TXF

15070c0d4284d17858832257033cc48c9dcf1f06
manel1954 <ea3eiz@ea3eiz.com>
2022-04-13 01:25:00 +0200

    1

--- Open_ejecutar_solodstar.sh
+++ Open_ejecutar_solodstar.sh
@@ -4,7 +4,7 @@
 cd /home/pi/MMDVMHost
 #/home/pi/B108/./qt_info_solodstar & sudo ircddbgateway -gui & sudo ./MMDVMDSTAR MMDVMDSTAR.ini 
 sed -i '13c SOLODSTAR=ON' /home/pi/status.ini
-sudo ./MMDVMHost MMDVMDSTAR.ini &
+sudo ./MMDVMDSTAR MMDVMDSTAR.ini &
 sleep 10
 sudo ircddbgateway -gui
 

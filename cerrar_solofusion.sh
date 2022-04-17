ecebb4520dce8d00cdea550bd2ce6f0399376461
manel1954 <ea3eiz@ea3eiz.com>
2022-04-18 00:27:58 +0200

    1

--- cerrar_solofusion.sh
+++ cerrar_solofusion.sh
@@ -5,10 +5,10 @@ SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_solofusion.desktop /home/pi
 
-sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version/;xterm -geometry 88x17+648+664 -bg black -fg white -fa 'Verdana' -fs 9x -T SOLOFUSION -e sudo sh OpenEjecutarSolofusion.sh'" /home/pi/Abrir_solofusion.desktop
-sed -i "7c Icon=/home/pi/$SCRIPTS_version/SOLO_YSF.png" /home/pi/Abrir_solofusion.desktop
-sed -i "11c Name[es_ES]=Abrir solo FUSION" /home/pi/Abrir_solofusion.desktop
-sed -i "12c SOLOFUSION=OFF" /home/pi/status.ini
+sudo sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version/;xterm -geometry 88x17+648+664 -bg black -fg white -fa 'Verdana' -fs 9x -T SOLOFUSION -e sudo sh OpenEjecutarSolofusion.sh'" /home/pi/Abrir_solofusion.desktop
+sudo sed -i "7c Icon=/home/pi/$SCRIPTS_version/SOLO_YSF.png" /home/pi/Abrir_solofusion.desktop
+sudo sed -i "11c Name[es_ES]=Abrir solo FUSION" /home/pi/Abrir_solofusion.desktop
+sudo sed -i "12c SOLOFUSION=OFF" /home/pi/status.ini
 
 cd /home/pi
 sudo cp Abrir_solofusion.desktop /home/pi/Desktop

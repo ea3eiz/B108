beed314712dba2eb5e1befeb8bf7135318145d2b
manel1954 <ea3eiz@ea3eiz.com>
2022-04-18 13:41:25 +0200

    1

--- cerrar_ESPECIAL.sh
+++ cerrar_ESPECIAL.sh
@@ -4,7 +4,8 @@ cd /home/pi/Desktop
 sudo cp Abrir_ESPECIAL.desktop /home/pi
 #sleep 1
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
-sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=88x19 --title ESPECIAL -e sudo sh ejecutar_ESPECIAL.sh'" /home/pi/Abrir_ESPECIAL.desktop
+sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version;xterm -geometry 88x17+648+664 -bg black -fg white -fa 'Verdana' -fs 9x -T ESPECIAL -e sudo sh ejecutar_ESPECIAL.sh'" /home/pi/Abrir_ESPECIAL.desktop
+
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_ESPECIAL_OFF.png" /home/pi/Abrir_ESPECIAL.desktop
 sed -i "10c Name[es_ES]=Abrir Especial" /home/pi/Abrir_ESPECIAL.desktop
 sed -i "10c MMDVMESPECIAL=OFF" /home/pi/status.ini
@@ -12,5 +13,5 @@ sed -i "10c MMDVMESPECIAL=OFF" /home/pi/status.ini
 cd /home/pi
 sudo cp Abrir_ESPECIAL.desktop /home/pi/Desktop
 #sleep 1
-sudo rm /home/pi/Abrir_ESPECIAL.desktop
+sudo rm /home/pi/Abrir_ESPECIAL.desktop 
 

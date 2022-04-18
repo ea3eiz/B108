a2676333894a6afe0e2d3e0f3fd49253a6796876
manel1954 <ea3eiz@ea3eiz.com>
2022-04-18 13:41:38 +0200

    1

--- ejecutar_ESPECIAL.sh
+++ ejecutar_ESPECIAL.sh
@@ -29,7 +29,7 @@ sudo cp /home/pi/RXF_ESPECIAL.desktop /home/pi/Desktop
 #sleep 1
 sudo rm /home/pi/RXF_ESPECIAL.desktop
 
-#Escribe en el fichero INFO_RXF para poner los datos en el icono INFO TXF                        
+#Escribe en el fichero INFO_RXF para poner los datos en el icono INFO TXF                         
 sed -i "3c $frecuencia" /home/pi/INFO_RXF
 
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
@@ -55,7 +55,7 @@ sleep 2
 sudo ./MMDVMESPECIAL MMDVMESPECIAL.ini
 cd /home/pi/Desktop
 sudo cp Abrir_ESPECIAL.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; lxterminal --geometry=88x19 --title=ESPECIAL -e sudo sh ejecutar_ESPECIAL.sh'" /home/pi/Abrir_ESPECIAL.desktop
+sed -i "4cExec=sh -c 'cd /home/pi/$SCRIPTS_version;xterm -geometry 88x17+648+664 -bg black -fg white -fa 'Verdana' -fs 9x -T ESPECIAL -e sudo sh ejecutar_ESPECIAL.sh'" /home/pi/Abrir_ESPECIAL.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_ESPECIAL_OFF.png" /home/pi/Abrir_ESPECIAL.desktop
 sed -i "10c Name[es_ES]=Abrir Especial" /home/pi/Abrir_ESPECIAL.desktop
 

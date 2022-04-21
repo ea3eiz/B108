eb07834ae40dc447324f9b110abf546879493219
manel1954 <ea3eiz@ea3eiz.com>
2022-04-21 16:21:05 +0200

    1

--- ejecutar_DMR2NXDN.sh
+++ ejecutar_DMR2NXDN.sh
@@ -54,7 +54,7 @@ sed -i "15c $frecuencia" /home/pi/INFO_RXF
 SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_DMR2NXDN.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh cerrar_DMR2NXDN.sh'" /home/pi/Abrir_DMR2NXDN.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version/;sudo sh cerrar_DMR2NXDN.sh'" /home/pi/Abrir_DMR2NXDN.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DMR2NXDN_ON.png" /home/pi/Abrir_DMR2NXDN.desktop
 sed -i "10c Name[es_ES]=Cerrar DMR2NXDN" /home/pi/Abrir_DMR2NXDN.desktop
 sed -i "16c DMR2NXDN=ON" /home/pi/status.ini
@@ -65,19 +65,20 @@ sudo rm /home/pi/Abrir_DMR2NXDN.desktop
 
 cd /home/pi/DMR2NXDN
 
-sudo lxterminal --geometry=75x12 -e ./DMR2NXDN DMR2NXDN.ini & 
-cd /home/pi/MMDVMHost
-#/home/pi/B108/./qt_info_dmr2nxdn & sudo lxterminal --geometry=75x12 -e ./DMR2NXDN MMDVMDMR2NXDN.ini &
-sudo lxterminal --geometry=75x12 -e ./DMR2NXDN MMDVMDMR2NXDN.ini &
+sudo lxterminal --geometry=88x12 -e ./DMR2NXDN DMR2NXDN.ini & 
 
+cd /home/pi/MMDVMHost
+xterm -geometry 88x17+648+664 -bg black -fg white -fa 'Verdana' -fs 9x -T MMDVMDMR2NXDN -e ./DMR2NXDN MMDVMDMR2NXDN.ini &
 cd /home/pi/NXDNClients/NXDNGateway
-sudo ./NXDNGateway NXDNGateway.ini
+
+
+xterm -geometry 88x12+648+136 -bg black -fg white -fa 'Verdana' -fs 9x -T NXDNGateway -e ./NXDNGateway NXDNGateway.ini
 
 cd /home/pi/Desktop
 sudo cp Abrir_DMR2NXDN.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; xterm -geometry 88x17+648+664 -bg black -fg white -fa 'Verdana' -fs 9x -T DMR2NXDN -e sudo sh ejecutar_DMR2NXDN.sh'" /home/pi/Abrir_DMR2NXDN.desktop
-sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DMR2NXDN_OFF.png" /home/pi/Abrir_DMR2NXDN.desktop
-sed -i "10c Name[es_ES]=Abrir DMR2NXDN" /home/pi/Abrir_DMR2NXDN.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; xterm -geometry 88x17+648+664 -bg black -fg white -fa 'Verdana' -fs 9x -T DMR2NXDN -e sudo sh cerrar_DMR2NXDN.sh'" /home/pi/Abrir_DMR2NXDN.desktop
+sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_DMR2NXDN_ON.png" /home/pi/Abrir_DMR2NXDN.desktop
+sed -i "10c Name[es_ES]=Cerrar DMR2NXDN" /home/pi/Abrir_DMR2NXDN.desktop
 sed -i "16c DMR2NXDN=OFF" /home/pi/status.ini
 cd /home/pi
 sudo cp Abrir_DMR2NXDN.desktop /home/pi/Desktop

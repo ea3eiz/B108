cde00594be938f7fdfbf83f9a5025a62bd170ac0
manel1954 <ea3eiz@ea3eiz.com>
2022-04-24 17:17:20 +0200

    1

--- cerrar_autoarranque.sh
+++ cerrar_autoarranque.sh
@@ -6,7 +6,7 @@ sudo cp Menu_Autoarranque.desktop /home/pi
 #sed -i "4c DV4mini=ON" /home/pi/status.ini
 sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh ejecutar_autoarranque.sh'" /home/pi/Menu_Autoarranque.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_AUTOARRANQUE1.png" /home/pi/Menu_Autoarranque.desktop
-sed -i "11c Name[es_ES]=Abrir Autoarranque" /home/pi/Menu_Autoarranque.desktop
+sed -i "11c Name[es_ES]=Visualizar Autoarranque" /home/pi/Menu_Autoarranque.desktop
 cd /home/pi
 sudo cp Menu_Autoarranque.desktop /home/pi/Desktop
 sleep 1

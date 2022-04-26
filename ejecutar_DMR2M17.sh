0b20f8b13298b095b7e8ad94414d21e15f107bc1
manel1954 <ea3eiz@ea3eiz.com>
2022-04-26 11:54:33 +0200

    1

--- ejecutar_DMR2M17.sh
+++ ejecutar_DMR2M17.sh
@@ -19,6 +19,6 @@ cd /home/pi/DMR2M17
 sudo lxterminal --geometry=75x12 -e ./DMR2M17 DMR2M17.ini & 
 
 cd /home/pi/MMDVMHost
-sudo ./MMDVMDMR2M17 MMDVMDMR2M17.ini
+xterm -geometry 88x17+1272+664 -bg black -fg white -fa 'Verdana' -fs 9x -T MMDVMDMR2M17 -e ./MMDVMDMR2M17 MMDVMDMR2M17.ini
 
   

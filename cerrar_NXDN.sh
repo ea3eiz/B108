c9e4fcd296df646223c1a82aefcbe1a0abda469a
manel1954 <ea3eiz@ea3eiz.com>
2022-04-29 23:53:53 +0200

    1

--- cerrar_NXDN.sh
+++ cerrar_NXDN.sh
@@ -5,7 +5,7 @@ SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp Abrir_NXDN.desktop /home/pi
 
-sed -i "4c Exec=sh -c 'cd /home/pi/B108/;xterm -geometry 88x17+1272+664 -bg black -fg white -fa 'Verdana' -fs 9x -T MMDVMNXDX -e sudo sh ejecutar_NXDN.sh'" /home/pi/Abrir_NXDN.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/B108/;xterm -geometry 88x17+650+664 -bg black -fg white -fa 'Verdana' -fs 9x -T MMDVMNXDX -e sudo sh ejecutar_NXDN.sh'" /home/pi/Abrir_NXDN.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_NXDN_OFF.png" /home/pi/Abrir_NXDN.desktop
 sed -i "10c Name[es_ES]=Abrir NXDN" /home/pi/Abrir_NXDN.desktop
 sed -i "17c NXDN=OFF" /home/pi/status.ini

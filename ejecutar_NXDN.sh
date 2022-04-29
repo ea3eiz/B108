75ddc604ec8eca980aefc27bac8f6a6cf0c27ffd
manel1954 <ea3eiz@ea3eiz.com>
2022-04-29 23:25:00 +0200

    1

--- ejecutar_NXDN.sh
+++ ejecutar_NXDN.sh
@@ -37,13 +37,13 @@ sudo rm /home/pi/Abrir_NXDN.desktop
 
 cd /home/pi/MMDVMHost
 #/home/pi/B108/./qt_info_nxdn & sudo lxterminal --geometry=80x12 -e ./MMDVMNXDN MMDVMNXDN.ini &
-sudo lxterminal --geometry=80x12 -e ./MMDVMNXDN MMDVMNXDN.ini &
+sudo ./MMDVMNXDN MMDVMNXDN.ini &
 cd /home/pi/NXDNClients/NXDNGateway
-sudo ./NXDNGateway NXDNGateway.ini
+sudo lxterminal --geometry=88x12 -e ./NXDNGateway NXDNGateway.ini
 
 cd /home/pi/Desktop
 sudo cp Abrir_NXDN.desktop /home/pi
-sed -i "4c Exec=sh -c 'cd /home/pi/$SCRIPTS_version;lxterminal --geometry=75x15 -e sudo sh ejecutar_NXDN.sh'" /home/pi/Abrir_NXDN.desktop
+sed -i "4c Exec=sh -c 'cd /home/pi/B108/;xterm -geometry 88x17+1272+664 -bg black -fg white -fa 'Verdana' -fs 9x -T MMDVMNXDX -e sudo sh ejecutar_NXDN.sh'" /home/pi/Abrir_NXDN.desktop
 sed -i "5c Icon=/home/pi/$SCRIPTS_version/ICONO_NXDN_OFF.png" /home/pi/Abrir_NXDN.desktop
 sed -i "10c Name[es_ES]=Abrir NXDN" /home/pi/Abrir_NXDN.desktop
 sed -i "17c NXDN=OFF" /home/pi/status.ini
